cdd2b4e6f815a7898bc2fcd052589363f7366f24
Setlistr <jesse@setlistr.ai>
2026-04-07 11:57:58 -0500

    Create GoogleAnalytics.tsx

---
+++ setlistr-2/components/GoogleAnalytics.tsx
@@ -0,0 +1,27 @@
+// setlistr-2/components/GoogleAnalytics.tsx
+// Drop this file in your components folder, then add <GoogleAnalytics /> to your root layout
+
+import Script from 'next/script'
+
+const GA_ID = 'G-7R24S26V1J'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  )
+}
