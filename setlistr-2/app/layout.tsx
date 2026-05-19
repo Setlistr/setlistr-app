38c3dfa22e1ec30625fd995cb5ce98382088eac5
Songlister <jesse.slack.music@gmail.com>
2026-05-19 11:29:13 -0500

    Add reb2b tracking script to root layout head

--- setlistr-2/app/layout.tsx
+++ setlistr-2/app/layout.tsx
@@ -18,6 +18,9 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `!function(key) {if (window.reb2b) return;window.reb2b = {loaded: true};var s = document.createElement("script");s.async = true;s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);}("1N5W0H7VG8O5");` }} />
+      </head>
       <body>
         <GoogleAnalytics />
         {children}
